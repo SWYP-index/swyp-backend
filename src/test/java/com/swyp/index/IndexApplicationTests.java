2e0835bb39da5467f901729ae8b83f90578ca2f1
zerochani <youngchan0510@gmail.com>
2025-07-05 13:23:15 +0900

    프로젝트 초기 구조 세팅

---
+++ src/test/java/com/swyp/index/IndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swyp.index;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
