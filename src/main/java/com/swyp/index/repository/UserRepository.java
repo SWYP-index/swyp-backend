b5386999485ee7a5c9c203d63802b6618e13bc7a
zerochani <youngchan0510@gmail.com>
2025-07-06 19:50:10 +0900

    UserRepository 추가

---
+++ src/main/java/com/swyp/index/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.swyp.index.repository;
+
+import com.swyp.index.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
