727a2e637c7c891fe96c6cea8be5485f13ab7c52
이동주 <leednj0113@naver.com>
2025-07-24 17:43:41 +0900

    fix: 쿠키 HTTPS 적용

--- src/main/java/com/swyp/index/global/common/CookieUtil.java
+++ src/main/java/com/swyp/index/global/common/CookieUtil.java
@@ -4,24 +4,24 @@ import java.time.Duration;
 
 import org.springframework.http.ResponseCookie;
 
-import jakarta.servlet.http.Cookie;
-
 public class CookieUtil {
 
 	public static ResponseCookie createRefreshTokenCookie(String refreshToken) {
 		return ResponseCookie.from("refreshToken", refreshToken)
 			.path("/")
 			.httpOnly(true)
+			.secure(true)
 			.maxAge(Duration.ofDays(7))
-			.sameSite("none")         // SameSite 옵션 설정
+			.sameSite("none")
 			.build();
 	}
 
 	public static ResponseCookie createAccessTokenCookie(String accessToken) {
 		return ResponseCookie.from("accessToken", accessToken)
 			.path("/")
+			.secure(true)
 			.maxAge(Duration.ofDays(30))
-			.sameSite("Lax")         // SameSite 옵션 설정
+			.sameSite("none")
 			.build();
 	}
 }
