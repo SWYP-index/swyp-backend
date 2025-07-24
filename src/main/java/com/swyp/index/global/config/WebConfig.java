e67f468b8a26c235fcb2590f8f267b5abefae8f4
이동주 <leednj0113@naver.com>
2025-07-24 23:31:39 +0900

    fix: CORS 오픈

--- src/main/java/com/swyp/index/global/config/WebConfig.java
+++ src/main/java/com/swyp/index/global/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
 
-		config.setAllowedOrigins(List.of("http://localhost:3000"));
+		config.setAllowedOrigins(List.of("*"));
 		config.addAllowedMethod("*");
 		config.addAllowedHeader("*");
 		config.setAllowCredentials(true);
