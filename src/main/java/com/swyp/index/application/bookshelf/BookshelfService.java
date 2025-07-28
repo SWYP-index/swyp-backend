c477ad2242a2e39a616dd43fa914850e1147d703
youngchan park <youngchanpark@youngchanui-MacBookAir.local>
2025-07-29 01:33:45 +0900

    refactor: 팩토리 메서드 사용 강화를 위해 @Builder 접근 제어 설정

--- src/main/java/com/swyp/index/application/bookshelf/BookshelfService.java
+++ src/main/java/com/swyp/index/application/bookshelf/BookshelfService.java
@@ -22,15 +22,16 @@ public class BookshelfService {
     private final BookRepository bookRepository;
     // 사용자의 책상에 새로운 책을 READING 상태로 추가.
     public Bookshelf addBookToBookshelf(Long userId, String isbn) {
-        // 1. 사용자(User)와 책(Book) 엔티티를 조회합니다.
+        //  사용자(User)와 책(Book) 엔티티를 조회합니다.
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
         Book book = bookRepository.findByIsbn(isbn)
                 .orElseThrow(() -> new IllegalArgumentException("해당 ISBN의 책을 찾을 수 없습니다."));
 
-        if(bookshelfRepository.findByUserAndBook(user, book).isPresent()){
+        //  중복 추가 방지
+        bookshelfRepository.findByUserAndBook(user, book).ifPresent(shelf -> {
             throw new IllegalStateException("이미 책장에 추가된 책입니다.");
-        }
+        });
 
         Bookshelf shelf = Bookshelf.startReading(user, book);
         return bookshelfRepository.save(shelf);
