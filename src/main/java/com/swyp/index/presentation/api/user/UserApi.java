d4683050630585d41df602bd82b2072695183e2d
이동주 <leednj0113@naver.com>
2025-07-29 15:39:55 +0900

    fix: CustomPrincipal 파라미터로 변경

--- src/main/java/com/swyp/index/presentation/api/user/UserApi.java
+++ src/main/java/com/swyp/index/presentation/api/user/UserApi.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,9 +51,7 @@ public class UserApi {
 	})
 	@SecurityRequirement(name = "JWT Authentication") //이 API는 JWt 인증이 필요함.
 	@GetMapping("/me")
-	public ResponseEntity<?> getUser(Authentication authentication, HttpServletResponse response) {
-		CustomPrincipal principal = (CustomPrincipal) authentication.getPrincipal();
-
+	public ResponseEntity<?> getUser(@AuthenticationPrincipal CustomPrincipal principal, HttpServletResponse response) {
 		User user = userRepository.findById(principal.getId())
 			.orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
