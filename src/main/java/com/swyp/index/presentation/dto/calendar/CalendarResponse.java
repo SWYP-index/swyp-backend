6348590ad0d245d914d669727cfdf59915598aa2
youngchan park <youngchanpark@youngchanui-MacBookAir.local>
2025-07-31 03:29:39 +0900

    feat: 스웨거 추가

--- src/main/java/com/swyp/index/presentation/dto/calendar/CalendarResponse.java
+++ src/main/java/com/swyp/index/presentation/dto/calendar/CalendarResponse.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Set;
 
@@ -14,11 +13,13 @@ import java.util.Set;
 @Schema(description = "월별 캘린더 독서 활동 응답 DTO")
 public class CalendarResponse {
 
-    @Schema(description = "독서 시작일 목록")
+    @Schema(description = "독서 시작일 목록", example = "[\"2025-07-09\", \"2025-07-22\"]")
     private Set<LocalDate> startDates;
-    @Schema(description = "독서 완료일 목록")
+
+    @Schema(description = "독서 완료일 목록", example = "[\"2025-07-17\"]")
     private Set<LocalDate> finishDates;
 
-    @Schema(description = "날짜별 기록된 감정 이름 목록")
+    @Schema(description = "날짜별 기록된 감정 이름 목록",
+            example = "{\"2025-07-11\":[\"감동\",\"위로\"], \"2025-07-12\":[\"슬픔\"]}")
     private Map<LocalDate, Set<String>> emotionsByDate;
 }
