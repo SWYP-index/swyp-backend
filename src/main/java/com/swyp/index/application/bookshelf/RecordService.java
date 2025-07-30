3feaede6f47c89bdac419eca4a528140757ca91e
youngchan park <youngchanpark@youngchanui-MacBookAir.local>
2025-07-30 16:25:52 +0900

    예외 상황에 대한 에러코드와 처리 로직 추가

--- src/main/java/com/swyp/index/application/bookshelf/RecordService.java
+++ src/main/java/com/swyp/index/application/bookshelf/RecordService.java
@@ -7,6 +7,8 @@ import com.swyp.index.domain.bookshelf.PageRecord;
 import com.swyp.index.domain.bookshelf.ReadingStatus;
 import com.swyp.index.domain.bookshelf.RecordEmotion;
 import com.swyp.index.domain.user.User;
+import com.swyp.index.global.exception.CustomException;
+import com.swyp.index.global.exception.ErrorCode;
 import com.swyp.index.infrastructure.repository.*;
 import com.swyp.index.presentation.dto.record.RecordCreateRequest;
 import lombok.RequiredArgsConstructor;
@@ -29,7 +31,20 @@ public class RecordService {
     //새로운 페이지 기록을 생성
     //사용자와 책에 대한 Bookshelf가 없으면 새로 생성하고, 있으면 기존 bookshelf에 기록 추가
     public PageRecord createPageRecord(Long userId, RecordCreateRequest request) {
-        // 1. 요청에 필요한 사용자, 책 엔티티를 조회합니다.
+
+        //유효성 검증 로직 추가
+        if(request.getEmotions() == null || request.getEmotions().isEmpty()){
+            throw new CustomException(ErrorCode.EMOTIONS_NOT_PROVIED);
+        }
+        //내용도 없고 감정도 없는 경우
+        boolean isContentEmpty = request.getContent() == null || request.getContent().isBlank();
+        boolean areEmotionsEmpty = request.getEmotions().isEmpty();
+
+        if(isContentEmpty && areEmotionsEmpty){
+            throw new CustomException(ErrorCode.EMPTY_RECORD_DATA);
+        }
+
+        // 요청에 필요한 사용자, 책 엔티티를 조회합니다.
         User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
         Book book = bookRepository.findByIsbn(request.getIsbn()).orElseThrow(() -> new IllegalArgumentException("해당 ISBN의 책을 찾을 수 없습니다."));
 
--- src/main/java/com/swyp/index/global/exception/ErrorCode.java
+++ src/main/java/com/swyp/index/global/exception/ErrorCode.java
@@ -15,6 +15,9 @@ public enum ErrorCode {
 	INVALID_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "이메일 인증 코드가 일치하지 않습니다."),
 	CANNOT_RECORD_FINISHED_BOOK(HttpStatus.BAD_REQUEST, "이미 완독한 책에는 기록을 추가할 수 없습니다."),
 	INVALID_STATUS_UPDATE(HttpStatus.BAD_REQUEST, "유효하지 않은 상태 변경 요청입니다."),
+	INVALID_DATE_PARAMETER(HttpStatus.BAD_REQUEST, "유효하지 않은 연도 또는 월입니다."),
+	EMOTIONS_NOT_PROVIED(HttpStatus.BAD_REQUEST, "하나 이상의 감정을 등록해야 합니다."),
+	EMPTY_RECORD_DATA(HttpStatus.BAD_REQUEST, "기록할 내용 또는 감정이 없습니다."),
 
 	// 401
 	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
@@ -31,6 +34,7 @@ public enum ErrorCode {
 	BOOK_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 책을 찾을 수 없습니다."),
 	BOOKSHELF_NOT_FOUND(HttpStatus.NOT_FOUND, "책장에 해당 책이 존재하지 않습니다."),
 	RECORD_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 기록을 찾을 수 없습니다."),
+	EMOTION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 감정을 찾을 수 없습니다."),
 
 	// 409
 	DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다."),
