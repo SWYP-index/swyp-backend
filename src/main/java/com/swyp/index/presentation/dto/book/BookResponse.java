18b339c8346d1d00e1bda3d8a6db6dc861be43e3
이동주 <leednj0113@naver.com>
2025-08-01 14:31:55 +0900

    style: 상태 DTO null 가시성 추가

--- src/main/java/com/swyp/index/presentation/dto/book/BookResponse.java
+++ src/main/java/com/swyp/index/presentation/dto/book/BookResponse.java
@@ -5,6 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record BookResponse(
-	@Schema(description = "사용자의 해당 도서 상태", example = "NULL, WISH, READING, FINISHED") String status,
+	@Schema(description = "사용자의 해당 도서 상태", example = "null, WISH, READING, FINISHED") String status,
 	@JsonProperty("booK") BookDto bookDto) {
 }
