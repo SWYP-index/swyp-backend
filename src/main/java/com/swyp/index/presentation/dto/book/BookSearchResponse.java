5525ad034987faaf034d7a8e092082711e240d90
이동주 <leednj0113@naver.com>
2025-08-03 16:29:04 +0900

    Update BookSearchResponse.java

--- src/main/java/com/swyp/index/presentation/dto/book/BookSearchResponse.java
+++ src/main/java/com/swyp/index/presentation/dto/book/BookSearchResponse.java
@@ -18,14 +18,14 @@ public class BookSearchResponse {
 	private int page;
 
 	@Schema(description = "총 검색 결과 수", example = "162")
-	private long totalResult;
+	private long totalResults;
 
 	private List<BookDto> books;
 
 	public static BookSearchResponse empty() {
 		return BookSearchResponse.builder()
 			.page(0)
-			.totalResult(0)
+			.totalResults(0)
 			.books(List.of())
 			.build();
 	}
