adc773a6e520bdafb945111d986708dd551416a6
이동주 <leednj0113@naver.com>
2025-08-03 16:16:18 +0900

    fix: 감정 검색 스웨거 캐시 삭제

--- src/main/java/com/swyp/index/presentation/dto/book/BookSearchResponse.java
+++ src/main/java/com/swyp/index/presentation/dto/book/BookSearchResponse.java
@@ -18,14 +18,14 @@ public class BookSearchResponse {
 	private int page;
 
 	@Schema(description = "총 검색 결과 수", example = "162")
-	private long totalResults;
+	private long totalResult;
 
 	private List<BookDto> books;
 
 	public static BookSearchResponse empty() {
 		return BookSearchResponse.builder()
 			.page(0)
-			.totalResults(0)
+			.totalResult(0)
 			.books(List.of())
 			.build();
 	}
