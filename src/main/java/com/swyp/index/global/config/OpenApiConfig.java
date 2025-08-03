165aa3d10259c214865e7733f941f898d0f440c9
이동주 <leednj0113@naver.com>
2025-08-03 12:57:59 +0900

    fix: 스웨거 인증 주소 추가

--- src/main/java/com/swyp/index/global/config/OpenApiConfig.java
+++ src/main/java/com/swyp/index/global/config/OpenApiConfig.java
@@ -41,5 +41,4 @@ public class OpenApiConfig {
                 .components(compnents);
 
     }
-
 }
--- src/main/java/com/swyp/index/global/config/SecurityConfig.java
+++ src/main/java/com/swyp/index/global/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
 			.cors(cors -> cors.configurationSource(corsConfigurationSource))
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(authorize -> authorize
-				.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+				.requestMatchers("/swagger-ui/**", "/v3/api-docs/**" , "/swagger/**").permitAll()
 				.requestMatchers("/api/users/**", "/api/bookshelf/**", "/api/desk/**", "/api/books/**",
 					"/api/reports/**", "/api/addBookshelf/**").authenticated()
 				.anyRequest().permitAll())
