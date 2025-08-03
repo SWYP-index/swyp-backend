5e4b696a09c6209f0f0eaa7ab3986e99d1d401e8
이동주 <leednj0113@naver.com>
2025-08-03 15:18:33 +0900

    fix: data.sql 감정 쿼리문 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,26 +5,47 @@
 -- =================================================================
 
 -- Emotion (ID 1~20 고정)
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (1, '감동', 'POSITIVE', 'YOUR_S3_BASE_URL/positive_gamdong.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (2, '설렘', 'POSITIVE', 'YOUR_S3_BASE_URL/positive_seollem.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (3, '유쾌한', 'POSITIVE', 'YOUR_S3_BASE_URL/positive_yukwaehan.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (4, '공감', 'POSITIVE', 'YOUR_S3_BASE_URL/positive_gonggam.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (5, '위로', 'POSITIVE', 'YOUR_S3_BASE_URL/positive_wiro.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (6, '슬픔', 'NEGATIVE', 'YOUR_S3_BASE_URL/negative_seulpeum.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (7, '분노', 'NEGATIVE', 'YOUR_S3_BASE_URL/negative_bunno.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (8, '혼란', 'NEGATIVE', 'YOUR_S3_BASE_URL/negative_honlan.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (9, '불쾌한', 'NEGATIVE', 'YOUR_S3_BASE_URL/negative_bulkwaehan.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (10, '공포', 'NEGATIVE', 'YOUR_S3_BASE_URL/negative_gongpo.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (11, '놀람', 'NEUTRAL', 'YOUR_S3_BASE_URL/neutral_nollam.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (12, '당황한', 'NEUTRAL', 'YOUR_S3_BASE_URL/neutral_danghwanghan.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (13, '답답한', 'NEUTRAL', 'YOUR_S3_BASE_URL/neutral_dapdaphan.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (14, '아쉬운', 'NEUTRAL', 'YOUR_S3_BASE_URL/neutral_aswium.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (15, '어색한', 'NEUTRAL', 'YOUR_S3_BASE_URL/neutral_eosaekhan.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (16, '깨달음', 'THOUGHT', 'YOUR_S3_BASE_URL/thought_ggaedar-eum.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (17, '통찰', 'THOUGHT', 'YOUR_S3_BASE_URL/thought_tongchal.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (18, '의문', 'THOUGHT', 'YOUR_S3_BASE_URL/thought_uimun.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (19, '영감', 'THOUGHT', 'YOUR_S3_BASE_URL/thought_yeonggam.png');
-INSERT INTO emotion (id, name, category, icon_image_url) VALUES (20, '성찰', 'THOUGHT', 'YOUR_S3_BASE_URL/thought_seongchal.png');
+INSERT INTO emotion (id, name, category) VALUES (1, '설렘', 'POSITIVE');
+INSERT INTO emotion (id, name, category) VALUES (2, '감동', 'POSITIVE');
+INSERT INTO emotion (id, name, category) VALUES (3, '유쾌한', 'POSITIVE');
+INSERT INTO emotion (id, name, category) VALUES (4, '공감', 'POSITIVE');
+INSERT INTO emotion (id, name, category) VALUES (5, '위로', 'POSITIVE');
+INSERT INTO emotion (id, name, category) VALUES (6, '불쾌한', 'NEGATIVE');
+INSERT INTO emotion (id, name, category) VALUES (7, '슬픔', 'NEGATIVE');
+INSERT INTO emotion (id, name, category) VALUES (8, '분노', 'NEGATIVE');
+INSERT INTO emotion (id, name, category) VALUES (9, '혼란', 'NEGATIVE');
+INSERT INTO emotion (id, name, category) VALUES (10, '공포', 'NEGATIVE');
+INSERT INTO emotion (id, name, category) VALUES (11, '어색한', 'NEUTRAL');
+INSERT INTO emotion (id, name, category) VALUES (12, '놀람', 'NEUTRAL');
+INSERT INTO emotion (id, name, category) VALUES (13, '당황한', 'NEUTRAL');
+INSERT INTO emotion (id, name, category) VALUES (14, '답답한', 'NEUTRAL');
+INSERT INTO emotion (id, name, category) VALUES (15, '아쉬움', 'NEUTRAL');
+INSERT INTO emotion (id, name, category) VALUES (16, '통찰', 'THOUGHT');
+INSERT INTO emotion (id, name, category) VALUES (17, '깨달음', 'THOUGHT');
+INSERT INTO emotion (id, name, category) VALUES (18, '의문', 'THOUGHT');
+INSERT INTO emotion (id, name, category) VALUES (19, '영감', 'THOUGHT');
+INSERT INTO emotion (id, name, category) VALUES (20, '성찰', 'THOUGHT');
+
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (1, '감동', 'POSITIVE', 'YOUR_S3_BASE_URL/positive_gamdong.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (2, '설렘', 'POSITIVE', 'YOUR_S3_BASE_URL/positive_seollem.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (3, '유쾌한', 'POSITIVE', 'YOUR_S3_BASE_URL/positive_yukwaehan.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (4, '공감', 'POSITIVE', 'YOUR_S3_BASE_URL/positive_gonggam.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (5, '위로', 'POSITIVE', 'YOUR_S3_BASE_URL/positive_wiro.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (6, '슬픔', 'NEGATIVE', 'YOUR_S3_BASE_URL/negative_seulpeum.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (7, '분노', 'NEGATIVE', 'YOUR_S3_BASE_URL/negative_bunno.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (8, '혼란', 'NEGATIVE', 'YOUR_S3_BASE_URL/negative_honlan.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (9, '불쾌한', 'NEGATIVE', 'YOUR_S3_BASE_URL/negative_bulkwaehan.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (10, '공포', 'NEGATIVE', 'YOUR_S3_BASE_URL/negative_gongpo.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (11, '놀람', 'NEUTRAL', 'YOUR_S3_BASE_URL/neutral_nollam.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (12, '당황한', 'NEUTRAL', 'YOUR_S3_BASE_URL/neutral_danghwanghan.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (13, '답답한', 'NEUTRAL', 'YOUR_S3_BASE_URL/neutral_dapdaphan.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (14, '아쉬운', 'NEUTRAL', 'YOUR_S3_BASE_URL/neutral_aswium.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (15, '어색한', 'NEUTRAL', 'YOUR_S3_BASE_URL/neutral_eosaekhan.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (16, '깨달음', 'THOUGHT', 'YOUR_S3_BASE_URL/thought_ggaedar-eum.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (17, '통찰', 'THOUGHT', 'YOUR_S3_BASE_URL/thought_tongchal.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (18, '의문', 'THOUGHT', 'YOUR_S3_BASE_URL/thought_uimun.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (19, '영감', 'THOUGHT', 'YOUR_S3_BASE_URL/thought_yeonggam.png');
+--INSERT INTO emotion (id, name, category, icon_image_url) VALUES (20, '성찰', 'THOUGHT', 'YOUR_S3_BASE_URL/thought_seongchal.png');
 
 
 -- =================================================================
