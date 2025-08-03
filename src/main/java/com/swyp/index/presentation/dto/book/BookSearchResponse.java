c3aed827a4264e7d024bb732cab1014a39ec0fc3
이동주 <leednj0113@naver.com>
2025-08-03 17:02:28 +0900

    Update BookSearchResponse.java

--- src/main/java/com/swyp/index/presentation/dto/book/BookSearchResponse.java
+++ src/main/java/com/swyp/index/presentation/dto/book/BookSearchResponse.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "책 검색 결과 응답 DTO")
 public class BookSearchResponse {
 
 	@Schema(description = "현재 페이지 번호", example = "2")
