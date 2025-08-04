c4915d7c0ee2f1836e1bddbc3b7600a4f4d9c22a
youngchanpark <youngchan0510@gmail.com>
2025-08-04 19:13:19 +0900

    fix: isbn,감정 필수,나머지 필드 null로 request 수정

--- src/main/java/com/swyp/index/presentation/dto/record/CompletionRecordCreateRequest.java
+++ src/main/java/com/swyp/index/presentation/dto/record/CompletionRecordCreateRequest.java
@@ -2,6 +2,7 @@ package com.swyp.index.presentation.dto.record;
 
 import com.swyp.index.domain.bookshelf.ReadingStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,15 +17,18 @@ public class CompletionRecordCreateRequest {
     @Schema(description = "도서 ISBN", example = "9791191114225", requiredMode = Schema.RequiredMode.REQUIRED)
     private String isbn;
 
+    @Nullable
     @Size(max = 1000, message = "내용은 1000자 이하로 입력해주세요.")
-    @Schema(description = "인상 깊은 구절 및 느낀 점 (1000자 이내)", example = "주인공의 선택이 인상 깊었다.")
+    @Schema(description = "인상 깊은 구절 및 느낀 점 (1000자 이내)", example = "주인공의 선택이 인상 깊었다.", requiredMode=Schema.RequiredMode.NOT_REQUIRED,nullable = true)
     private String content;
 
+    @Nullable
     @Size(max = 1500, message = "종합 감상평은 1500자 이하로 입력해주세요.")
-    @Schema(description = "종합 감상평 (1500자 이내)", example = "오랜만에 깊은 울림을 주는 책이었다.")
+    @Schema(description = "종합 감상평 (1500자 이내)", example = "오랜만에 깊은 울림을 주는 책이었다.", requiredMode=Schema.RequiredMode.NOT_REQUIRED,nullable = true)
     private String finalNote;
 
     @NotEmpty(message = "감정은 최소 1개 이상 선택해야 합니다.")
-    @Schema(description = "독서 기록에 대한 감정 목록 (최소 1개 이상 선택)")
+    @Size(max = 5, message = "감정은 최대 5개까지 선택 할 수 있습니다.")
+    @Schema(description = "독서 기록에 대한 감정 목록 (1~5개)", requiredMode=Schema.RequiredMode.REQUIRED)
     private List<EmotionDto> emotions;
 }
--- src/main/java/com/swyp/index/presentation/dto/record/PageRecordCreateRequest.java
+++ src/main/java/com/swyp/index/presentation/dto/record/PageRecordCreateRequest.java
@@ -1,6 +1,7 @@
 package com.swyp.index.presentation.dto.record;
 import com.swyp.index.domain.bookshelf.ReadingStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,16 +16,19 @@ public class PageRecordCreateRequest {
     @Schema(description = "도서 ISBN", example = "9791191114225", requiredMode = Schema.RequiredMode.REQUIRED)
     private String isbn;
 
+    @Nullable
     @NotNull(message = "페이지는 필수입니다.")
-    @Schema(description = "기록하는 페이지 번호", example = "123", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(description = "기록하는 페이지 번호", example = "123", requiredMode = Schema.RequiredMode.NOT_REQUIRED,nullable = true)
     private Integer page;
 
+    @Nullable
     @Size(max = 1000, message = "내용은 1000자 이하로 입력해주세요.")
-    @Schema(description = "인상 깊은 구절 및 느낀 점 (1000자 이내)", example = "주인공의 선택이 인상 깊었다.")
+    @Schema(description = "인상 깊은 구절 및 느낀 점 (1000자 이내)", example = "주인공의 선택이 인상 깊었다.",requiredMode = Schema.RequiredMode.NOT_REQUIRED,nullable = true)
     private String content;
 
     @NotEmpty(message = "감정은 최소 1개 이상 선택해야 합니다.")
-    @Schema(description = "독서 기록에 대한 감정 목록 (최소 1개 이상 선택)")
+    @Size(max = 5, message = "감정은 최대 5개까지 선택 할 수 있습니다.")
+    @Schema(description = "독서 기록에 대한 감정 목록 (1~5개)", requiredMode=Schema.RequiredMode.REQUIRED)
     private List<EmotionDto> emotions;
 
 }
